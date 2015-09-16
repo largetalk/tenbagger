7fd8153784485783469527e34c01c2fe917c1aea
Arthur <arthur_wang@adsame.com>
2015-09-16 15:42:32 +0800

    add more quote code

--- gather/quote/fetch_yahoo.py
+++ gather/quote/fetch_yahoo.py
@@ -24,19 +24,26 @@ def worker(code):
     print 'start process %s.%s' % (code, m)
     target_file = os.path.join(DATA_DIR, '%s.csv' % code)
     if os.path.exists(target_file):
-        print '%s quote file already exists'
+        print '%s quote file already exists' % code
     else:
-        cmd = "wget %s%s.%s -O %s" % (WGET_URL, code, m, target_file)
+        cmd = "wget -t 10 %s%s.%s -O %s" % (WGET_URL, code, m, target_file)
         print 'fetch data : %s' % cmd
         sub = Popen([cmd], stdout=PIPE, shell=True)
         out, err = sub.communicate()
         print out.rstrip()
+
+def clean_empty():
+    for f in os.listdir(DATA_DIR):
+        if f.endswith('.csv') and os.path.getsize(os.path.join(DATA_DIR, f)) < 10:
+            print 'clean emtpy file ', f
+            os.remove(os.path.join(DATA_DIR, f))
 
 def run():
     with open(STOCK_FILE, 'rb') as f:
         for code in f:
             pool.spawn(worker, code.strip())
     pool.join()
+    clean_empty()
 
 
 if '__main__' == __name__:
@@ -44,4 +51,4 @@ if '__main__' == __name__:
     start_at = time.time()
     print 'begin at: %s' % start_at
     run()
-    print 'end at:% %s and used %s' %s (time.time(), time.time() - start_at)
+    print 'end at: %s and used %s' % (time.time(), time.time() - start_at)
