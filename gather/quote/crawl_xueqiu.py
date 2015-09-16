511fe9ab9e28efaeb1bd983a9f62cd9dbb6a9080
Arthur <arthur_wang@adsame.com>
2015-09-16 23:56:16 +0800

    add crawl profit

--- gather/quote/crawl_xueqiu.py
+++ gather/quote/crawl_xueqiu.py
@@ -17,8 +17,10 @@ POOL_NUM = 2
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STOCK_FILE = os.path.join(BASE_DIR, "stock.txt")
 COMPANY_DIR = os.path.join(BASE_DIR, 'company')
-#COMPANY_URL = 'http://xueqiu.com/S/%s/GSJJ'
 COMPANY_URL = 'http://xueqiu.com/stock/f10/compinfo.json?symbol=%s&page=1&size=4&_=%s'
+
+FINANCE_URL = 'http://xueqiu.com/stock/f10/finmainindex.json?symbol=%s&page=%s&size=4&_=%s'
+PROFIT_DIR = os.path.join(BASE_DIR, 'profit')
 
 REFER_URL = 'http://xueqiu.com/S/%s/GSJJ'
 UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36'
@@ -36,7 +38,7 @@ def judgeMarket(code):
 
 def crawl_company(code):
     m = judgeMarket(code)
-    print 'start process %s%s' % (m, code)
+    print 'start process company %s%s' % (m, code)
     target_file = os.path.join(COMPANY_DIR, '%s.csv' % code)
     if os.path.exists(target_file):
         print '%s company file already exists' % code
@@ -50,32 +52,72 @@ def crawl_company(code):
             status = resp.code
             content = resp.read()
             print 'crawl %s status: %s' % (code, status)
-            dumps(code, target_file, content)
+            dumps_company(code, target_file, content)
         except Exception, e:
             print 'crawl %s err: %s' % (code, str(e))
 
-def dumps(code, file, data):
+def dumps_company(code, file, data):
     try:
         print 'dumps %s file' % code
         #dic = json.loads(data)
-        with open(file, 'w+') as f:
+        with open(file, 'w') as f:
             f.write(data)
     except Exception, e:
         print 'dumps %s error: %s' % (code, str(e))
 
+def crawl_profit(code):
+    m = judgeMarket(code)
+    print 'start process profit %s%s' % (m, code)
+    target_file = os.path.join(PROFIT_DIR, '%s.csv' % code)
+    if os.path.exists(target_file):
+        print '%s profit file already exists' % code
+    else:
+        page = 1
+        headers = {'User-Agent': UA, 'Referer': REFER_URL % (m+code), "Cookie": COOKIE}
+        while True:
+            url = FINANCE_URL % (m + code, page, int(time.time() * 1000))
+            print 'crawl url : %s' % url
+            try:
+                req = urllib2.Request(url, headers=headers)
+                resp = urllib2.urlopen(req, timeout=10)
+                status = resp.code
+                content = resp.read()
+                if len(content) > 20:
+                    print 'dumps %s profit page %s' % (code, page)
+                    dumps_profit(code, target_file, content)
+                    page += 1
+                else:
+                    print 'crawl %s profit end, page %s' % (code, page)
+                    break
+            except Exception, e:
+                print 'crawl %s err: %s' % (code, str(e))
+                break
+
+def dumps_profit(code, file, data):
+    try:
+        with open(file, 'a') as f:
+            f.write(data)
+            f.write('\r\n')
+    except Exception, e:
+        print 'dumps %s profit error: %s' % (code, str(e))
+
 def clean_empty():
     for f in os.listdir(COMPANY_DIR):
         if f.endswith('.csv') and os.path.getsize(os.path.join(COMPANY_DIR, f)) < 10:
-            print 'clean emtpy file ', f
+            print 'clean emtpy company file ', f
             os.remove(os.path.join(COMPANY_DIR, f))
+    for f in os.listdir(PROFIT_DIR):
+        if f.endswith('.csv') and os.path.getsize(os.path.join(PROFIT_DIR, f)) < 10:
+            print 'clean emtpy profit file ', f
+            os.remove(os.path.join(PROFIT_DIR, f))
 
 def run():
     with open(STOCK_FILE, 'rb') as f:
         for code in f:
             pool.spawn(crawl_company, code.strip())
+            pool.spawn(crawl_profit, code.strip())
     pool.join()
     clean_empty()
-
 
 if '__main__' == __name__:
     import time
