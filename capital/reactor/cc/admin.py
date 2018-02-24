f8ea27ad8a1e93f106da8328f7fa491a70cfa43c
Arthur <arthur_wang@adsame.com>
2018-02-24 11:49:57 +0800

    change model

--- capital/reactor/cc/admin.py
+++ capital/reactor/cc/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CreditCard
+from .models import Pos
+from .models import CashOut
+
+
+class CreditCardAdmin(admin.ModelAdmin):
+        list_display = ('name', 'tail_no', 'bill_day', 'card_type', 'due_day', 'due_period')
+
+class PosAdmin(admin.ModelAdmin):
+        fields = ['rate']
+        list_display = ('id', 'rate')
+
+class CashOutAdmin(admin.ModelAdmin):
+        list_display = ('card', 'swipe_day', 'amount', 'due_day', 'isRepaid')
+
+admin.site.register(CreditCard, CreditCardAdmin)
+admin.site.register(Pos, PosAdmin)
+admin.site.register(CashOut, CashOutAdmin)
--- capital/reactor/cc/models.py
+++ capital/reactor/cc/models.py
@@ -4,14 +4,17 @@ from utils.bm import BaseModel
 from datetime import date
 
 
-_CARD_TYPE_CHOICES = (('A', 'A'), ('B', 'B'))
+_CARD_TYPE_CHOICES = (('A', '指定还款日'), ('B', '定长还款期'))
 class CreditCard(BaseModel):
     name = models.CharField(max_length=100)
     tail_no = models.IntegerField()
     bill_day = models.PositiveSmallIntegerField()
     card_type = models.CharField(max_length = 5, choices=_CARD_TYPE_CHOICES, default='A')
-    due_day = models.PositiveSmallIntegerField(null=True)
-    due_period = models.PositiveSmallIntegerField(null=True)
+    due_day = models.PositiveSmallIntegerField(null=True, blank=True)
+    due_period = models.PositiveSmallIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return '%s(%s)' % (self.name, self.tail_no)
 
 
 class Pos(models.Model):
@@ -26,6 +29,6 @@ class CashOut(BaseModel):
     fee = models.DecimalField(max_digits=9, decimal_places=2)
     due_day = models.DateField() #到期日
     isRepaid = models.BooleanField(default=False)
-    pay_day = models.DateField() #实际还款日
-    apr = models.FloatField() #年利率
+    pay_day = models.DateField(null=True, blank=True) #实际还款日
+    apr = models.FloatField(null=True, blank=True) #年利率
 
