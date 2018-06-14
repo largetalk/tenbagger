8c9831a6bc8f7cddbe6de8e482bbacc731409b08
Arthur <arthur_wang@adsame.com>
2018-06-14 21:34:47 +0800

    add admin

--- capital/reactor/cc/admin.py
+++ capital/reactor/cc/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from .models import CreditCard
 from .models import Pos
 from .models import CashOut
+from .models import Loans
 
 
 class CreditCardAdmin(admin.ModelAdmin):
@@ -16,6 +17,12 @@ class CashOutAdmin(admin.ModelAdmin):
         fields = ['card', 'swipe_day', 'amount', 'pos_rate', 'isRepaid']
         list_display = ('card', 'swipe_day', 'amount', 'fee', 'due_day', 'isRepaid', 'pay_day', 'apr')
 
+class LoansAdmin(admin.ModelAdmin):
+        fields = ['bank', 'loan_day', 'amount', 'loan_type', 'due_day', 'apr']
+        list_display = ('bank', 'loan_day', 'amount', 'loan_type', 'due_day', 'apr')
+
+
 admin.site.register(CreditCard, CreditCardAdmin)
 admin.site.register(Pos, PosAdmin)
 admin.site.register(CashOut, CashOutAdmin)
+admin.site.register(Loans, LoansAdmin)
