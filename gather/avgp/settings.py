7db8c738d17c4a8f76e846d05fe7badc05eb5d8f
Arthur <arthur_wang@adsame.com>
2019-04-29 12:08:59 +0800

    add localsettings

--- gather/avgp/settings.py
+++ gather/avgp/settings.py
@@ -1,8 +1,12 @@
 #tushare_token = "adsfadfa"
-tushare_token = "fcdef551aef99cd1b5fab6fe1b0b389092ddafdd5fbb477a4fbb2a71"
+tushare_token = "336156685e93e322618582d2f4f54c72ea024dfdd3be6e9b7763341c"
 
 #db_url = 'sqlite:///:memory:'
-#db_url = 'mysql+mysqldb://avgp:avgp@localhost:3306/avgp?charset=utf8'
-db_url = 'mysql+mysqldb://avgp:avgp@121.199.4.41:3306/avgp?charset=utf8'
+db_url = 'mysql+mysqldb://avgp:avgp@localhost:3306/avgp?charset=utf8'
 DB_ECHO = False
 DB_ECHO_POOL = False
+
+try:
+    from localsettings import *
+except:
+    pass
